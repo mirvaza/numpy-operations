88f02a5865c86eaa8ff5c4696aa6c9ec1bb4854d
mirvaza <mirvazam@gmail.com>
2026-02-24 06:35:20 +0530

    Improve formatting and comments in attendance.py
    Refined formatting and comments for better readability.

--- attendance.py
+++ attendance.py
@@ -8,25 +8,23 @@ print("Original Attendance Matrix:")
 print(attendance)
 
 #Deleting  1 month
-attendance = np.delete(attendance, 0, axis=1)
-print("After Deleting Month 1:")
+attendance=np.delete(attendance, 0, axis=1)
+print("After deleting 1st month :")
 print(attendance)
 
-#Broadcasting (Add 5% Grace)
-attendance = attendance + 5
-print("\nAfter Adding 5% Grace Attendance:")
+#Broadcasting (Add 5% grace)
+attendance = attendance+5
+print("\nAfter Adding 5% grace attendance:")
 print(attendance)
 
 
-# Slicing: accessing student 2 details
-student2 = attendance[1, :]
+# Slicing:accessing student 2 details
+student2=attendance[1, :]
 print("\nAttendance of Student 2:")
 print(student2)
 
 
 #reshaping: Converting to Single Row Format
-# ---------------------------------
 single_row = attendance.reshape(1, -1)
-
 print("\nFinal Single Row Format:")
 print(single_row)
